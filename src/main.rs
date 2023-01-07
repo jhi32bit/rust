c455b219216f9197da56af5feeda70c24db0d63e
Ruebezahl <ruebezahl1997@gmail.com>
2023-01-07 13:33:57 +0100

    guessing random number

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,26 @@
+extern crate rand;
+
 use std::io;
-//use rand::Rng;
+use std::cmp::Ordering;
+use rand::Rng;
 
 fn main() {
-    let err_read = "Failed to read line.";
-
-    // let secret_number = rand::thread_rng().gen_range(1..=100);
-    
-    println!("Guess the number!\nPlease insert your guess");
-    
+    println!("########### GAME ##########");
+    // generate random number
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    // initialize empty String
     let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess).expect(err_read);
-
-    println!("You guessed: {guess}");
-}
-
-# [test]
-fn should_fail() {
-    unimplemented!();
+    println!("Insert a number (1-100):");
+    // get user input String
+    io::stdin()
+        .read_line(&mut guess)
+        .expect("Failed to read line");
+    // parse String to i32
+    let num_guess: i32 = guess.trim().parse().expect("Please type a number!");
+    // check result
+    match num_guess.cmp(&secret_number) {
+        Ordering::Less => print!("Sorry, your number {guess} is smaller than {secret_number}!"),
+        Ordering::Greater => print!("Sorry, your number {guess} is bigger than {secret_number}!"),
+        Ordering::Equal => print!("Yeah! You guessed the right number..."),
+    }
 }
